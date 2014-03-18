f1f40efd3431740b5c8b76fe75b51289300be1a5
fo95116 <fo95116@gmail.com>
2014-03-18 12:53:27 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Precipitation::Application.config.secret_token = 'd48f84662438904711fca1497348249cd65cfe35ec20810517a390b4e1f908a457bb0d798d256b2a01b2272fd92af7b7e92cfcd9910e9acae1c93b1a08874599'
