ef1ec381362ec2ef751bbc71ec75cdd77518ccf8
fo95116 <fo95116@gmail.com>
2014-03-18 15:46:33 -0700

    geolocation is working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,34 +14,65 @@
 //= require jquery_ujs
 //= require_tree .
 
-$(function() {
-  var $city = $('#city');
-  var $state = $('#state');
 
-  $('input[name="commit"]').click(function(event) {
-    event.preventDefault(); // this will prevent a post to the server
+  function geoFindMe() {
+    var output = document.getElementById("out");
 
-    $city = $city.val().trim().replace(/\s+/g, "_");
-    // return the value of the city and if ther is any spaces they will be replaced with a '_' for ajax request
-    $state = $state.val().trim().toUpperCase();
+    if (!navigator.geolocation){
+      // navigator.geolocation is the object that publishes the geolocation API
+      // if the API is not supported it will output an error
+      output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+      return;
+    }
 
-    // console.log($city);
-    // console.log($state);
+    function success(position) {
+      var latitude  = position.coords.latitude;
+      var longitude = position.coords.longitude;
 
-    $.ajax({
-      url : "http://api.wunderground.com/api/441472960cf74c21/conditions/q/"+ $state + "/" + $city + ".json",
-      dataType : "jsonp",
-      success : function(parsed_json) {
-        var location = parsed_json['current_observation']['display_location']['city']
-        var temp = parsed_json['current_observation']['temp_f'];
-        var weather = parsed_json['current_observation']['weather']
+      output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+    };
 
-        console.log(location);
-        console.log(temp);
-        console.log(weather);
-      }
-    });// end of ajax request
+    function error() {
+      output.innerHTML = "Unable to retrieve your location";
+    };
 
-  }); // end of input action
+    output.innerHTML = "<p>Locating…</p>";
 
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+
+$(document).ready(function() {
+  geoFindMe();
 });
+// $(function() {
+//   var $city = $('#city');
+//   var $state = $('#state');
+
+//   $('input[name="commit"]').click(function(event) {
+//     event.preventDefault(); // this will prevent a post to the server
+
+//     $city = $city.val().trim().replace(/\s+/g, "_");
+//     // return the value of the city and if ther is any spaces they will be replaced with a '_' for ajax request
+//     $state = $state.val().trim().toUpperCase();
+
+//     // console.log($city);
+//     // console.log($state);
+
+//     $.ajax({
+//       url : "http://api.wunderground.com/api/441472960cf74c21/conditions/q/"+ $state + "/" + $city + ".json",
+//       dataType : "jsonp",
+//       success : function(parsed_json) {
+//         var location = parsed_json['current_observation']['display_location']['city']
+//         var temp = parsed_json['current_observation']['temp_f'];
+//         var weather = parsed_json['current_observation']['weather']
+
+//         console.log(location);
+//         console.log(temp);
+//         console.log(weather);
+//       } // end of ajax request
+//     });// end of ajax request
+
+//   }); // end of input action
+
+// });
