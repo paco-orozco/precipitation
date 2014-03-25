bda8c89da8fc5d6e6097ac399a6658a390a6635a
fo95116 <fo95116@gmail.com>
2014-03-25 10:42:38 -0700

    changed icon size

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -79,43 +79,43 @@ $(function() {
             // +++++++++++++++++++ parse through our arrays to determine the icons
             for(var i=0; i<green.length; i++){
               if(weather == green[i]) {
-                $('#action').append('<img src="assets/running-256.png"/>');
-                $('#climate').append('<img src="assets/partly_cloudy_day-256.png"/>');
+                $('#action').append('<img src="assets/running-128.png"/>');
+                $('#climate').append('<img src="assets/partly_cloudy_day-128.png"/>');
               }
             }
             // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
             for(var j=0; j<red.length; j++){
               if(weather == red[j]) {
-                $('#action').append('<img src="assets/livingroom-256.png"/>');
-                $('#climate').append('<img src="assets/rain-256.png"/>');
+                $('#action').append('<img src="assets/livingroom-128.png"/>');
+                $('#climate').append('<img src="assets/rain-128.png"/>');
               }
             }
             // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
             for(var k=0; k<danger.length; k++){
               if(weather == danger[k]) {
-                $('#action').append('<img src="assets/self_distruct_button-256.png"/>');
-                $('#climate').append('<img src="assets/explosion-256.png"/>');
+                $('#action').append('<img src="assets/self_distruct_button-128.png"/>');
+                $('#climate').append('<img src="assets/explosion-128.png"/>');
               }
             }
             // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
             for(var l=0; l<visibility.length; l++){
               if(weather == visibility[l]) {
-                $('#action').append('<img src="assets/flashlight-256.png"/>');
-                $('#climate').append('<img src="assets/fog_night-256.png"/>');
+                $('#action').append('<img src="assets/flashlight-128.png"/>');
+                $('#climate').append('<img src="assets/fog_night-128.png"/>');
               }
             }
             // ++++++++++++++++++++++ end of initial append +++++++++++++++++++++
             if(temp > 70 && temp < 98) {
-              $('#clothing').html('<img src="assets/t_shirt-256.png"/>')
+              $('#clothing').html('<img src="assets/t_shirt-128.png"/>')
             }
             else if(temp > 45 && temp < 70) {
-              $('#clothing').html('<img src="assets/jumper-256.png"/>')
+              $('#clothing').html('<img src="assets/jumper-128.png"/>')
             }
             else if(temp < 45) {
-              $('#clothing').html('<img src="assets/jacket-256.png"/>')
+              $('#clothing').html('<img src="assets/jacket-128.png"/>')
             }
             else if(temp > 98) {
-              $('#clothing').html('<img src="assets/shorts-256.png"/>')
+              $('#clothing').html('<img src="assets/shorts-128.png"/>')
             }
            //++++++++++++++++++++++++++++++++++++++++++++++++
           }
@@ -161,43 +161,43 @@ $(function() {
               // +++++++++++++++++++ parse through our arrays to determine the icons
               for(var i=0; i<green.length; i++){
                 if(weather == green[i]) {
-                  $('#action').append('<img src="assets/running-256.png"/>');
-                  $('#climate').append('<img src="assets/partly_cloudy_day-256.png"/>');
+                  $('#action').append('<img src="assets/running-128.png"/>');
+                  $('#climate').append('<img src="assets/partly_cloudy_day-128.png"/>');
                 }
               }
               // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
               for(var j=0; j<red.length; j++){
                 if(weather == red[j]) {
-                  $('#action').append('<img src="assets/livingroom-256.png"/>');
-                  $('#climate').append('<img src="assets/rain-256.png"/>');
+                  $('#action').append('<img src="assets/livingroom-128.png"/>');
+                  $('#climate').append('<img src="assets/rain-128.png"/>');
                 }
               }
               // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
               for(var k=0; k<danger.length; k++){
                 if(weather == danger[k]) {
-                  $('#action').append('<img src="assets/self_distruct_button-256.png"/>');
-                  $('#climate').append('<img src="assets/explosion-256.png"/>');
+                  $('#action').append('<img src="assets/self_distruct_button-128.png"/>');
+                  $('#climate').append('<img src="assets/explosion-128.png"/>');
                 }
               }
               // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
               for(var l=0; l<visibility.length; l++){
                 if(weather == visibility[l]) {
-                  $('#action').append('<img src="assets/flashlight-256.png"/>');
-                  $('#climate').append('<img src="assets/fog_night-256.png"/>');
+                  $('#action').append('<img src="assets/flashlight-128.png"/>');
+                  $('#climate').append('<img src="assets/fog_night-128.png"/>');
                 }
               }
               // ++++++++++++++++++++++ end of initial append +++++++++++++++++++++
               if(temp > 70 && temp < 98) {
-                $('#clothing').html('<img src="assets/t_shirt-256.png"/>')
+                $('#clothing').html('<img src="assets/t_shirt-128.png"/>')
               }
               else if(temp > 45 && temp < 70) {
-                $('#clothing').html('<img src="assets/jumper-256.png"/>')
+                $('#clothing').html('<img src="assets/jumper-128.png"/>')
               }
               else if(temp < 45) {
-                $('#clothing').html('<img src="assets/jacket-256.png"/>')
+                $('#clothing').html('<img src="assets/jacket-128.png"/>')
               }
               else if(temp > 98) {
-                $('#clothing').html('<img src="assets/shorts-256.png"/>')
+                $('#clothing').html('<img src="assets/shorts-128.png"/>')
               }
              //++++++++++++++++++++++++++++++++++++++++++++++++
             }
